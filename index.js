36476c8749bad260f47402d043e79486b5548fee
lauriemcgee <livingoncoffeeandflowers@gmail.com>
2017-07-19 16:27:11 -0500

    angular working

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hello world');
+var angular = require('angular');
+
+var myApp = angular.module('weatherApp', []);
+
+myApp.controller('weatherCtrl', function($scope, $http) {
+  var vm = $scope;
+  vm.channelInfo = {
+    heading: 'Rocketmiles OpenWeather App',
+    subheading1: 'Looking for the weather?'
+  };
+});
