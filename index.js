9aa5a1c626a3f1f4db5c10382dad2418141e32ba
lauriemcgee <livingoncoffeeandflowers@gmail.com>
2017-07-25 11:41:16 -0500

    Weather now manually populates on page load, and updates if we're able to grab your location--still need 5 day forecast

--- index.js
+++ index.js
@@ -1,36 +1,109 @@
 var angular = require('angular');
+angular.module("weatherApp", [])
 
-var myApp = angular.module('weatherApp', []);
+// SET UP A FACTORY FOR RECIEVING DEFERRED GEOLOCATION PROMISE
 
-// Creating the module's controller with data to ensure it works
+  .factory('getLocation', ['$q', '$window',  function($q, $window) {
+    var currentLocation = {};
 
-myApp.controller('weatherCtrl', function($scope, $http) {
-  var vm = $scope;
-  vm.channelInfo = {
-    heading: 'Rocketmiles OpenWeather App',
-    subheading1: 'Looking for the weather?'
-  };
+    currentLocation.getCurrentPosition = function() {
+     
+      var deferred = $q.defer();
+      
+      if (!$window.navigator.geolocation) {
+        deferred.reject('Geolocation not supported');
+     
+      } else {
+        
+        $window.navigator.geolocation.getCurrentPosition(
+          
+          function(position) {
+            deferred.resolve(position);
+          },
+          
+          function(err) {
+            deferred.reject(err);
+          });
 
-// Getting the user's latitude and longitude, building url for api request
+      }
+      return deferred.promise;
+    };
+    return currentLocation;
+    
+  }])
 
-  window.navigator.geolocation.getCurrentPosition(function(data) {
-    console.log(data);
-    vm.lat = parseFloat(data.coords.latitude);
-    vm.lon = parseFloat(data.coords.longitude);
-    var apiKey = '0dd8bb79c45efccc5e6993ee4b787e33';
-    var openWeatherUrl = 'http://api.openweathermap.org/data/2.5/weather?lat=' + vm.lat + '&lon=' + vm.lon + '&appid=' + apiKey + '&units=imperial';
+  // SET UP A FACTORY FOR RECIEVING DEFERRED HTTP PROMISE, DEPENDENT ON RECIEVING GEOLOCATION
 
-  // Making the api call-storing response data
+  .factory('makeWeatherRequest', ['getLocation', '$http', '$q', function(getLocation, $http, $q) {
+    var currentWeather = {};
+    
 
-    $http.get(openWeatherUrl).then(function(responseWeather) {
-      console.log(responseWeather);
-      vm.weatherTemp = responseWeather.data.main.temp;
-      vm.humidity = responseWeather.data.main.humidity;
-      vm.cloudCover = responseWeather.data.weather[0].description;
-      console.log(vm.cloudCover);
-      console.log(vm.weatherTemp);
-      console.log(vm.humidity);
+    currentWeather.getCurrentWeather = function(lat, lon) {
+      var apiKey = '0dd8bb79c45efccc5e6993ee4b787e33';
+      var openWeatherUrl = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey + '&units=imperial';
+      
 
+      var deferred = $q.defer();
+      
+
+      if (!$http.get(openWeatherUrl)) {
+        deferred.reject('Something went wrong! Sorry!');
+      
+
+      } else {
+      
+
+        $http.get(openWeatherUrl).then(
+     
+          function successCallback(position) {
+            deferred.resolve(position);
+          },
+     
+          function errorCallback(err) {
+            deferred.reject(err);
+          });
+      }
+      
+
+      return deferred.promise;
+    };
+    return currentWeather;
+  }])
+
+
+  // SET THE DATA TO THE SCOPE IN THE CONTROLLER, ENSURING HTTP IS COMPLETED AFTR GEOLOCATION
+
+.controller('WeatherCtrl', ['getLocation', 'makeWeatherRequest', '$scope', function(getLocation, makeWeatherRequest, $scope) {
+  getLocation.getCurrentPosition().then(function(positionData) {
+    $scope.lat = parseFloat(positionData.coords.latitude);
+    $scope.lon = parseFloat(positionData.coords.longitude);
+    console.log($scope.lat);
+    console.log($scope.lon);
+  },
+  function(err) {
+    console.log(err); //User Denid Location -> Set Manually
+    $scope.lat = 41.12345;
+    $scope.lon = -87.123456;
+  }).then(function() {
+    makeWeatherRequest.getCurrentWeather($scope.lat, $scope.lon).then(locationData => {
+      console.log(locationData);
+      $scope.weatherTemp = locationData.data.main.temp;
+      $scope.humidity = locationData.data.main.humidity;
+      $scope.cloudCover = locationData.data.weather[0].description;
+      console.log($scope.weatherTemp);
+      console.log($scope.humidity);
+      console.log($scope.cloudCover);
     });
   });
-});
+}]);
+
+
+
+
+
+      
+
+
+
+
+
